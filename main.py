d030677bacfafda71e1f3950c513769956c40976
MichaelVarela <michaelvvarela@gmail.com>
2024-08-25 14:40:53 -0400

    added another image

--- main.py
+++ main.py
@@ -4,19 +4,23 @@ from send_text import textPhone
 
 while True:
     try:
-        location = pyautogui.locateCenterOnScreen('accept_images/large_accept.PNG', confidence=0.4)
+        location = pyautogui.locateCenterOnScreen('accept_images/large_accept.PNG', confidence=0.5)
         break
     except:
         try:
-            location = pyautogui.locateCenterOnScreen('accept_images/medium_accept.PNG', confidence=0.4)
+            location = pyautogui.locateCenterOnScreen('accept_images/medium_accept.PNG', confidence=0.5)
             break
         except:
             try:
-                location = pyautogui.locateCenterOnScreen('accept_images/small_accept.PNG', confidence=0.4)
+                location = pyautogui.locateCenterOnScreen('accept_images/small_accept.PNG', confidence=0.5)
                 break
             except:
-                time.sleep(1)
-                print('slept')
+                try:
+                    location = pyautogui.locateCenterOnScreen('accept_images/real_accept.PNG', confidence=0.5)
+                    break
+                except:
+                    time.sleep(1)
+                    print('slept')
 
 pyautogui.moveTo(location)
 pyautogui.click()
